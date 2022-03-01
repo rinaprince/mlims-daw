bf04ffac9e30a036762a8fb1647437fe9326b270
Marina Checa <marinacheca11@gmail.com>
2022-03-01 08:10:11 +0100

    Tema 8

---
+++ Unit 8/Task 801/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -0,0 +1,45 @@
+/* Implement at h1 */
+
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+/* Message appears when you click */
+
+document.querySelector('html').addEventListener('click', function () {
+    alert('Ouch! Stop punching me!');
+});
+
+/* Change the image */
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+/* Welcome message */
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+}
