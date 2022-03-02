5aa789ad1528ebce5f6901006cbf6a57ae1c6186
Marina Checa <marinacheca11@gmail.com>
2022-03-02 09:19:35 +0100

    Task 802 corrected

--- Unit 8/Task 802/scripts/main.js
+++ Unit 8/Task 802/scripts/main.js
@@ -1,22 +1,37 @@
-//Mutiplication
+//Calculator
 
-let firstNumberInput = document.querySelector('#first_number'); //This is to get the value of the first number.
-console.log(firstNumberInput.value); //And this is to show the number in the console.
+function main() {
 
-let secondNumberInput = document.querySelector('#second_number'); //To get the value of the second number.
-console.log(secondNumberInput.value);
+    let firstNumberInput = document.querySelector('#first_number'); //This is to get the value of the first number.
+    console.log(firstNumberInput.value); //And this is to show the number in the console.
 
-let multiplyButton = document.querySelector('#btn_multiply'); //To get the value of the multiply button.
-multiplyButton.onclick = function(){
-    multiply(firstNumberInput.value, secondNumberInput.value); //To do an event. In this case to multiply.
-}
+    let secondNumberInput = document.querySelector('#second_number'); //To get the value of the second number.
+    console.log(secondNumberInput.value);
 
-let divideButton = document.querySelector('#btn_divide'); //To get the value of the divide button.
-divideButton.onclick = function(){
-    alert('You clicked on the button!'); //To do an event.
-}
+    let multiplyButton = document.querySelector('#btn_multiply'); //To get the value of the multiply button.
+    multiplyButton.onclick = multiply();
+    //multiply(firstNumberInput.value, secondNumberInput.value); //To do an event. In this case to multiply.
 
-function multiply(num1, num2){ //Funtion created to multiply
-    let result = num1 * num2;
-    return result;
+    let divideButton = document.querySelector('#btn_divide'); //To get the value of the divide button.
+    divideButton.onclick = divide();
+    //alert('You clicked on the button!'); //To do an event.
+
+    let divResult = document.querySelector('#result'); //Toget the value from the result.
+
+    function multiply() { //Funtion created to multiply
+        let firstNumber = Number(firstNumberInput.value);
+        let secondNumber = Number(secondNumberInput.value);
+
+        divResult.textContent = "Result: " + (firstNumber * secondNumber);
+    }
+
+    function divide() { //Funtion created to divide
+        let firstNumber = Number(firstNumberInput.value);
+        let secondNumber = Number(secondNumberInput.value);
+
+        divResult.textContent = "Result: " + (firstNumber / secondNumber);
+
+        multiplyButton.addEventListener('click', multiply);
+        divideButton.addEventListener('click', divide);
+    }
 }
