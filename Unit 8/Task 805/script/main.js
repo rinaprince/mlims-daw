d899b404fb3b96f45362002d1c9483bda3c89bc5
Marina Checa <marinacheca11@gmail.com>
2022-03-09 09:54:47 +0100

    Task 805 in progress

---
+++ Unit 8/Task 805/script/main.js
@@ -0,0 +1,18 @@
+//Create a random checklist.
+
+"use strict";
+
+function main(){
+
+
+    for (let i = 0; i < 10; i++){
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        document.body.append(checkbox);
+    }
+
+    let array = new Array(); //Declaration 
+
+}
+
+document.addEventListener('DOMContentLoaded', main);
