ea7b931d46d896e8500a70fbfcf8dd33ea66fef6
Marina Checa <marinacheca11@gmail.com>
2022-03-14 09:00:12 +0100

    Task 806 getting better

--- Unit 8/Task 806/806-dom/script/main.js
+++ Unit 8/Task 806/806-dom/script/main.js
@@ -3,7 +3,8 @@
 "use strict";
 
 function main() {
-
+    let elements = document.querySelectorAll('p'); //Contar els paràgrafs, treure numero de 'p'.
+    alert(elements.length); //Alerta que conta i mostra el 'p'.
 }
 
 document.addEventListener('DOMContentLoaded', main);
