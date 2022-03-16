4829c82a118a41e22ecd32d671126b7129b04e5e
Marina Checa <marinacheca11@gmail.com>
2022-03-16 09:34:29 +0100

    Task 808 in progress

---
+++ Unit 8/Task 808/script/books.js
@@ -0,0 +1,26 @@
+//Bookstore II
+
+"use strict";
+
+//Comportament amb el DOM
+
+function main(){
+    
+
+    //Bucle for per a recòrrer l'array books.
+    for (let i = 0; i < books.length; i++){
+        let book = books[i];
+        if (book.alreadyRead == true){
+            let message = document.createElement('p');
+            message.innerHTML = (" You already read " + book.title + " by " + book.author);
+            document.body.append(message);
+        }
+        else{
+            let message = document.createElement('p');
+            message.innerHTML = ("You still need to read " + book.title + " by " + book.author);
+            document.body.append(message);
+        }
+    } 
+}
+
+document.addEventListener('DOMContentLoaded', main);
---
+++ Unit 8/Task 808/script/main.js
@@ -0,0 +1,49 @@
+//Bookstore I
+
+"use strict";
+
+//Dades comuns
+
+//Creació d'objectes.
+let bookOne ={ 
+    title: "La sombra del viento",
+    author: "Carlos Ruiz Zafón",
+    publishedIn: 2001,
+    alreadyRead: true,
+};
+
+let bookTwo ={
+    title: "El juego del ángel",
+    author: "Carlos Ruiz Zafón",
+    publishedIn: 2008,
+    alreadyRead: true,
+};
+
+let bookThree ={
+    title: "El prisionero del cielo",
+    author: "Carlos Ruiz Zafón",
+    publishedIn: 2011,
+    alreadyRead: true,
+};
+
+let bookFour ={
+    title: "El laberinto de los espíritus",
+    author: "Carlos Ruiz Zafón",
+    publishedIn: 2016,
+    alreadyRead: true,
+};
+
+let bookFive ={
+    title: "El truco de los espejos",
+    author: "Agatha Christie",
+    publishedIn: 1952,
+    alreadyRead: false,
+};
+
+//Array dels llibres.
+let books =[
+    bookOne,
+    bookTwo,
+    bookThree,
+    bookFour,
+];
